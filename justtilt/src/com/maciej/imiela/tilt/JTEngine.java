69061806850915c42ec83749c6d82e46d1c1aad4
mimiela <mimiela@AMDC1275>
2012-09-25 15:01:53 +0200

    Creation of splash screen, and starting application in different thread

---
+++ justtilt/src/com/maciej/imiela/tilt/JTEngine.java
@@ -0,0 +1,5 @@
+package com.maciej.imiela.tilt;
+
+public class JTEngine {
+	public static final int GAME_THREAD_DELAY = 4000;
+}
---
+++ justtilt/src/com/maciej/imiela/tilt/JTMainMenu.java
@@ -0,0 +1,18 @@
+package com.maciej.imiela.tilt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class JTMainMenu extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
